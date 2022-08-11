04efd27a0d7fc288d60d4642df226246590c2855
Jerry applegarth <applegarth1630@gmail.com>
2022-08-11 08:06:24 -0700

    3.0 completed 189 part of the search screen

--- app/src/main/java/com/applecompose/weather/presentation/screens/search/SearchScreen.kt
+++ app/src/main/java/com/applecompose/weather/presentation/screens/search/SearchScreen.kt
@@ -1,22 +1,103 @@
 package com.applecompose.weather.presentation.screens.search
 
+import androidx.compose.foundation.MutatePriority
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.applecompose.weather.presentation.components.WeatherAppBar
 
 @Composable
 fun SearchScreen(navController: NavController) {
-	Scaffold(topBar = {}) {
+	Scaffold(topBar = {
+		WeatherAppBar(
+			title = "Search",
+			icon = Icons.Default.ArrowBack,
+
+			navController = navController,
+			isMainScreen = false,
+			){
+			navController.popBackStack()
+		}
+	}) {
 		Surface() {
-			Column {
-				Text(text = "Search Screen")
+			Column(
+				verticalArrangement = Arrangement.Center,
+				horizontalAlignment = Alignment.CenterHorizontally
+			) {
+
+
 
 			}
 
 		}
 
 	}
+}
+
+@Composable
+fun SearchBar(
+	onSearch: (String) -> Unit = {}
+) {
+	val searchQueryState = rememberSaveable{ mutableStateOf("") }
+	val focusManager = LocalFocusManager.current
+	val valid = remember(searchQueryState.value) {
+		searchQueryState.value.trim().isNotEmpty()
+	}
+	Column() {
+		CommonTextField(
+			valueState = searchQueryState,
+			placeholder = "Seattle",
+			onAction = KeyboardActions {
+
+			}
+
+		)
+		
+	}
+
+}
+
+@Composable
+fun CommonTextField(valueState: MutableState<String>,
+                    placeholder: String,
+                    keyboardType: KeyboardType = KeyboardType.Text,
+                    imeAction: ImeAction = ImeAction.Next,
+                    onAction: KeyboardActions = KeyboardActions.Default) {
+	OutlinedTextField(
+		value = valueState.value,
+		onValueChange = { valueState.value = it},
+		label = { Text(text = placeholder)},
+		maxLines = 1,
+		singleLine = true,
+		keyboardOptions = KeyboardOptions(keyboardType = keyboardType, imeAction = imeAction),
+		keyboardActions = onAction,
+		colors = TextFieldDefaults.outlinedTextFieldColors(
+			focusedBorderColor = Color.Blue,
+			cursorColor = Color.Black),
+		shape = RoundedCornerShape(16.dp),
+		modifier = Modifier
+			.fillMaxWidth()
+			.padding(start = 12.dp, end = 12.dp))
+
+
 }
